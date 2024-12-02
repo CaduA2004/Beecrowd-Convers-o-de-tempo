1eb82cfce8d2968ef781fec07621d2d0b33bbbb1
cadu <andradecadu14@gmail.com>
2024-12-02 18:15:53 -0300

    first commit

---
+++ conversao_tempo.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = int.Parse(Console.ReadLine());
+        
+        int horas = N / 3600;
+        int minutos = (N % 3600) / 60;
+        int segundos = N % 60;
+        
+        Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+    }
+
+}
